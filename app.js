e239daea84934018e25258996a9413b13b275deb
cometticristian <cometticristian@gmail.com>
2020-04-14 10:39:48 -0300

    termine

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-// Require de Express
-const express = __¿?__;
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
-// Require de FS
-const fs = __¿?__;
-
-// Ejecución de Express
-const app = __¿?__;
-
-// Levantando el Servidor en el puerto 3030
 app.listen(3030, () => console.log('Server running in 3030 port'));
 
-// Leyendo y parseando (en array) el contenido de heroes.json
 const heroes = JSON.parse(fs.readFileSync(__dirname + '/data/heroes.json', 'utf-8'));
 
-// Ruta Raíz / ➝ Home
-app.get('/', __¿?__);
-
-// Ruta /heroes ➝ se envía todo el array y Express lo parsea para el browser como JSON :D
-app.get('/heroes', (¿?) => {
-	res.send(ACÁ_DEBERÍAMOS_ENVIAR_A_TODOS_LOS_HÉROES);
+app.get('/', (req, res) => {
+	res.send('Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!.')
 });
 
-// Ruta /heroes/n ➝ se envía el nombre y profesión del héroe solicitado
-app.get('¿?', (¿?) => {
-	// Acá lo primero será encontrar al héroe que corresponda
-	let heroe = __¿?__;
-	
-	// Si se encuentra al héroe se envía el nombre y su profesión
-	// Si NO se encuentra se envía el mensaje de no encontrado
+app.get('/heroes', (req, res) => {
+	res.send(heroes);
 });
 
-// Ruta /heroes/n/bio ➝ se envía la bio del héroe solicitado
-app.get('¿?', (¿?) => {
-	// Acá lo primero será encontrar al héroe que corresponda
-	let heroe = __¿?__;
-
-	// Si NO se encuentra al héroe se envía un mensaje
-	// Si se encuentra al héroe:
-		// Se pregunta si vino el parámetro Y el valor esperado y se envía la información
-		// Si nó vino el parámetro se envía el mensaje de error
+app.get('/heroes/detalle/:id', (req, res) => {
+	let heroe = req.params.id;
+	let encontrado = false;
+	heroes.forEach(function (cadaHeroe) {
+		if(cadaHeroe.id == heroe) {
+			res.send('Hola, mi nombre es ' + cadaHeroe.nombre + ' y soy ' + cadaHeroe.profesion);
+			encontrado = true;
+		}
+	})
+	if (encontrado == false) {
+		res.send('No se puede encontrar el heroe requerido');
 	}
 });
 
-// Ruta Créditos
-// ¿?
+app.get('/heroes/bio/:id/:ok?', (req, res) => {
+	let encontrado = false;
+	let heroe = req.params.id;
+	let parametroResenia = req.params.ok;
+	heroes.forEach(function (cadaHeroe) {
+		if(cadaHeroe.id == heroe && parametroResenia != 'ok') {
+			res.send(cadaHeroe.nombre + ' ' + 'Lamento que no desees saber mas de mi :(');
+			encontrado = true;
+		}else if (cadaHeroe.id == heroe && parametroResenia == 'ok') {
+			res.send(cadaHeroe.nombre + ' ' + cadaHeroe.resenia);
+			encontrado = true;
+		}
+	})
+	if (encontrado ==  false) {
+		res.send('No encontramos un heroe para mostrarte su biografia');
+	}
+});
 
-// Ruta... ¿Pára qué sirve esto?
+app.get('/creditos', function () {
+	res.send('Hecho por Cristian Cometti');
+})
+
 app.get('*', (req, res) => {
 	res.status(404).send('404 not found. <br> ¡Houston, poseemos problemas!');
 });
